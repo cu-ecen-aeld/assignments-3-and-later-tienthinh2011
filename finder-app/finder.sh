89fc01b7639079c9c20c35fadbdd3a7cc33f3ab2
Thinh Nguyen <tienthinh2011@gmail.com>
2024-06-28 09:53:16 +0000

    remove echo finder.sh

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -8,7 +8,7 @@
 # where X is the number of files in the directory and all subdirectories and Y is the number of matching lines found in respective files, 
 # where a matching line refers to a line which contains searchstr (and may also contain additional content).
 # test string: thinhnt is thinhnt open thinhnt
-echo "finder.sh"
+# echo "finder.sh"
 if [ $# -lt 2 ]
 then 
     echo "number of arguments should be >= 2"
